8f1474451ca9e7e42a4d82c0ef825c546ce98cbb
jamesBeeProg <jamesbeeprog@gmail.com>
2023-03-23 18:10:50 -0400

    Finish scoreboard command

--- src/command.ts
+++ src/command.ts
@@ -20,5 +20,35 @@ export const scoreboard = {
     },
     players: {
         list: (target?: Selector) => `scoreboard players list ${target}`,
+        get: (target: Selector) => `scoreboard players get ${target}`,
+        set: (targets: Selector, objective: string, score: number) =>
+            `scoreboard players set ${targets} ${objective} ${score}`,
+        add: (targets: Selector, objective: string, score: number) =>
+            `scoreboard players add ${targets} ${objective} ${score}`,
+        remove: (targets: Selector, objective: string, score: number) =>
+            `scoreboard players remove ${targets} ${objective} ${score}`,
+        reset: (targets: Selector, objective?: string) =>
+            `scoreboard players reset ${targets} ${objective}`,
+        enable: (targets: Selector, objective: string) =>
+            `scoreboard players enable ${targets} ${objective}`,
+        operation: (
+            targets: Selector,
+            targetObjective: string,
+            operation: ScoreboardOperation,
+            source: Selector,
+            sourceObjective: string,
+        ) =>
+            `scoreboard players operation ${targets} ${targetObjective} ${operation} ${source} ${sourceObjective}`,
     },
 };
+
+export type ScoreboardOperation =
+    | '+='
+    | '-='
+    | '*='
+    | '/='
+    | '%='
+    | '='
+    | '<'
+    | '>'
+    | '><';
--- src/dev.ts
+++ src/dev.ts
@@ -1,8 +1,9 @@
 import { Datapack } from '.';
+import { scoreboard } from './command';
 import { e } from './selector';
 
 const datapack = new Datapack();
 const testing = datapack.get('testing');
 
 testing.functions.get('tick');
-console.log(e.type('foo').type.not('foo').toString());
+console.log(scoreboard.players.list(e.team('foo').team.not('bar')));
